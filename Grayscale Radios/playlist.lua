3231fad86f87aef56cc89ae13f5f4a09738947df
John Wieland <john.r.wieland@gmail.com>
2022-07-14 13:24:06 -0400

    Delete playlist.lua

--- Grayscale Radios/playlist.lua
+++
@@ -1,8 +0,0 @@
-title="Rock-N-Roll"
-playlist={
-{"Acrobatic Mode","acro",8},
-{"Signal Low","siglow",6},
-{"Flaps Down","flapdn",7},
-{"Trainer Mode Off","trnoff",10},
-{"Veriometer ON","vrion",5},
-}
