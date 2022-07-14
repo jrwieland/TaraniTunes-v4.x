ac2d360823f076f29c8bf6158186b058ea2abd77
John Wieland <john.r.wieland@gmail.com>
2022-07-14 10:46:30 -0400

    4.0 Itunes Code Update
    Massive Rewrite of all code to match the color screen

--- Grayscale Radios/iTunes.lua
+++ Grayscale Radios/iTunes.lua
@@ -1,138 +1,249 @@
---[[ 
-TaraniTunes
- Version 3.0
- This Advenced version is based off of the Original TaraniTunes 
-  http://github.com/GilDev/TaraniTunes
- By GilDev
- http://gildev.tk
-It was agreed by GilDev and I that both versions of the script (the original 
-and this advanced version) would be available for users but hosted separately.
+--[[  #########################################################################
+      # TaraniTunes v.4.0                                                     #
+      # B&W Radios                                                            #
+      # License GPLv3: http://www.gnu.org/licenses/gpl-3.0.html		            #
+      #                                                                       #
+      # This program is free software; you can redistribute it and/or modify  #
+      # it under the terms of the GNU General Public License version 3 as     #
+      # published by the Free Software Foundation.                            #
+      #                                                                       #
+      # This program is distributed in the hope that it will be useful        #
+      # but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+      # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+      # GNU General Public License for more details.                          #
+      #########################################################################--]]
 
-The majority of the new enhancements are due to the work of Exean https://github.com/exean
+--Locals
+local playingSong = 1
+local errorOccured = false
+local ver = getVersion() --OpenTX Version
+loadScript("/SOUNDS/lists/bubba_mix/playlist.lua")() --1st playlist in songList (see sample list below)
 
-----Playlist selector file-------
-Also be sure to read about automated playlist creation---]]
+--Playlists folders names i.e. /SONGS/lists/"foldername"  below are sample names
+local songList=
+{"bubba_mix",
+"classic_country",
+"country_rock",
+"easy",
+"fun_country",
+"todays",
+"funrock",
+"hard",
+"laid_back",
+"modern",
+"rock",
+"soundtracks",
+"garage",
+"upbeat",
+} 
 
-local fileToLoad="/SCRIPTS/TELEMETRY/iTunes_player.lua"
-local active = true
-local thisPage={}
-local page={}
--- Playlist Directories
-local playlists={
-	"/SOUNDS/lists/classic/playlist.lua",
-	"/SOUNDS/lists/funrock/playlist.lua",
-	"/SOUNDS/lists/garage/playlist.lua",
-	"/SOUNDS/lists/hard/playlist.lua",
-	"/SOUNDS/lists/rock/playlist.lua",
-	"/SOUNDS/lists/upbeat/playlist.lua",
-}
-
-function getSelectedPlaylistID()
-	local res = 1
-	--read selected playlist from config
-	local f = io.open("itunes.conf", "r")	
-	if f ~= nil then
-		res = tonumber(io.read(f, 4))
-		io.close(f)
-	end
-	return res;
+-- Initiate Program
+local function init()
+  model.setGlobalVariable(7,0,1)
+  model.setGlobalVariable(8,0,1)
+  shPresent =getValue("sh")
+  if shPresent== 0 then
+    sw1=98
+  elseif LCD_W==212 then
+    sw1=113   
+  else
+    sw1=104
+  end
+  model.setCustomFunction(62,{switch = sw1,func = 16,name = playlist[1][2]})--resets playing song on  to the 1st song on startup
+  return tunes
 end
 
-local playlistID = getSelectedPlaylistID()
-selectedPlaylistFile=playlists[playlistID] 
+-- error Control functions
+local function error(strings)
+  errorStrings = strings
+  errorOccured = true
+end
 
+--BackGround 
+local function background(tunes)
+end
 
-local function clearTable(t)
-  if type(t)=="table" then
-    for i,v in pairs(t) do
-      if type(v) == "table" then
-        clearTable(v)
-      end
-      t[i] = nil
+local function run(tunes)
+--Update Locals
+  lcd.clear()
+  local selection=playingSong
+  local long=playlist[playingSong][3]--do not change this value it is the length of the current song playing
+  local upTime=model.getTimer(2).value--do not change this value it is how long the current song has played
+  local batt = getValue("tx-voltage")
+  local datenow = getDateTime()		
+  local timemins = (string.format("%02d",datenow.min))
+  local fly=model.getTimer(1).value  --flight duration timer: 0=timer1, 1=timer2
+  local altN=getValue("Alt")--current altitude from telementry
+  local altM=getValue("Alt+")--max altitude from telementry
+  local shPresent =getValue("sh")
+  
+  -- LS60 Switch Number controled by Radio profile
+  if shPresent== 0 then
+    sw1=98
+  elseif LCD_W==212 then
+    sw1=113   
+  else
+    sw1=104
+  end
+
+--Math
+  function round(num, decimals)
+    local mult = 10^(decimals or 0)
+    return math.floor(num * mult + .5) / mult 
+  end
+
+--Current time, OpenTX Version & Model Name
+  lcd.drawText(1,10,datenow.hour12..":"..timemins..datenow.suffix,SMLSIZE)	
+  lcd.drawText(1,2,"OTX "..ver,SMLSIZE)
+  if LCD_W == 212 then
+    lcd.drawText(LCD_W/2-(string.len(model.getInfo().name)/2)*6,2,model.getInfo().name)
+  else
+    lcd.drawText(LCD_W/2-(string.len(model.getInfo().name)/2)*5,13,model.getInfo().name)  
+  end
+  
+  --Transmitter battery
+  lcd.drawText(14, 18,"TX "..round(batt,1).."v",PREC1+SMLSIZE)
+  
+  -- Altitude
+  lcd.drawText(1, 26,"Alt= "..round(altN,2).."/"..round(altM,2),SMLSIZE)
+
+--Flight Time
+  lcd.drawTimer(LCD_W-32, 20, fly, MIDSIZE)	
+  
+--rssi
+  lcd.drawText(LCD_W-51,1,"RSSI "..getValue("RSSI").."/"..getValue("RSSI-"))
+
+--Cell Voltage
+  lcd.drawChannel(LCD_W-(string.len("LCEL"))*6, 11,"LCEL", PREC2) --displays a custom telementry sensor (lowest cell)calculated from "cels" sensor
+
+--Music Display
+  local y = 23;
+--Playlist title
+  lcd.drawLine(0, y+10, LCD_W, y+10,SOLID,FORCE)
+  lcd.drawText(LCD_W/2-(string.len(title))/2*5, y+12, title, SMLSIZE)
+
+-- Progressbar
+  --local k = math.floor((upTime/long)*33)*10
+  lcd.drawLine(0, y+18, LCD_W, y+18,SOLID,FORCE)
+  lcd.drawFilledRectangle(20, y+11, math.floor((upTime/long)*(LCD_W-43)), 7)
+  lcd.drawTimer(1, y+12, upTime, SMLSIZE+INVERS)
+  lcd.drawTimer(LCD_W-22, y+12, long, SMLSIZE+INVERS)
+
+--Song display
+  if selection == #playlist then
+    lcd.drawText(6, y+19, playlist[selection - 1][1],SMLSIZE)
+    lcd.drawText(1, y+26, string.char(126) .. playlist[selection][1],SMLSIZE)
+    lcd.drawText(6, y+33, playlist[1][1],SMLSIZE)
+  else
+    if selection == 1 then	
+      lcd.drawText(6, y+19, playlist[#playlist][1],SMLSIZE)
+      lcd.drawText(1, y+26, string.char(126) .. playlist[selection][1],SMLSIZE)
+      lcd.drawText(6, y+33, playlist[selection + 1][1],SMLSIZE)  
+    else		
+      lcd.drawText(6, y+19, playlist[selection - 1][1],SMLSIZE)
+      lcd.drawText(1, y+26, string.char(126) .. playlist[selection][1], SMLSIZE)
+      lcd.drawText(6, y+33, playlist[selection + 1][1],SMLSIZE)
+    end 
+  end	
+
+-- Music Controls
+  listP = getValue("ls63")
+  listN = getValue("ls64")
+  prevS = getValue("ls61")
+  nextS = getValue("ls62")
+
+--song over
+  if model.getTimer(2).value >= long then
+    if playingSong == #playlist then
+      model.setGlobalVariable(7,0,1)
+      playingSong = model.getGlobalVariable(7,0)
+      flushAudio()
+      model.setCustomFunction(62,{switch = sw1,func = 16,name = playlist[playingSong][2]})
+      model.setTimer(2,{value=0})
+    else
+      flushAudio()
+      playingSong = playingSong +1
+      model.setGlobalVariable(7,0,playingSong)
+      model.setCustomFunction(62,{switch = sw1,func = 16,name = playlist[playingSong][2]})
+      model.setTimer(2,{value=0})
     end
   end
-  collectgarbage()
-  return t 
-end 
- 
-thisPage.init=function(...)
-  if active then
-    page=dofile(fileToLoad)
-    page.init(...)
-  end 
-  return true
-end 
-
-thisPage.background=function(...)
-  if active then
-    page.background(...) 
+  
+-- Next song
+  if nextS > -1 then
+    if not nextSongSwitchPressed then
+      if playingSong == #playlist then
+        model.setGlobalVariable(7,0,1)
+        playingSong = model.getGlobalVariable(7,0)	
+      else
+        playingSong = model.getGlobalVariable(7,0)
+      end
+      flushAudio()
+      model.setCustomFunction(62,{switch = sw1,func = 16,name = playlist[playingSong][2]})
+      model.setTimer(2,{value=0})
+    else
+      nextSongSwitchPressed = false
+    end	
   end
-  return true
+
+--Change Previous Playlist
+  if listP > -1 then
+    if not prevListSwitchPressed then
+      if model.getGlobalVariable(8,0)<= 0 then	
+        model.setGlobalVariable(8,0,#songList)
+        set2 = songList[model.getGlobalVariable(8,0)]
+        playingSong = 1
+      else
+        set2 = songList[model.getGlobalVariable(8,0)]
+        playingSong = 1
+      end
+      loadScript("/SOUNDS/lists/"..set2.."/playlist.lua")()
+      model.setCustomFunction(62,{switch = 98,func = 16,name = playlist[playingSong][2]})
+      model.setGlobalVariable(7,0,1)
+      flushAudio()
+      model.setTimer(2,{value=0})
+    else
+      prevListSwitchPressed = false
+    end
+  end
+
+--Change next Playlist
+  if listN > -1 then
+    if not nextListSwitchPressed then
+      if model.getGlobalVariable(8,0) >= #songList then
+        set2 = songList[1] 
+        model.setGlobalVariable(8,0,1)
+        playingSong = 1
+      else
+        set2 = songList[model.getGlobalVariable(8,0)]
+        playingSong = 1
+      end
+      loadScript("/SOUNDS/lists/"..set2.."/playlist.lua")()
+      model.setCustomFunction(62,{switch = sw1,func = 16,name = playlist[playingSong][2]})
+      model.setGlobalVariable(7,0,1)
+      flushAudio()
+      model.setTimer(2,{value=0})
+    else
+      nextListSwitchPressed = false
+    end
+  end
+  
+-- previous song
+  if prevS > -1 then
+    if not prevSongSwitchPressed then
+      if playingSong == 1 then
+        model.setGlobalVariable(7,0,#playlist)
+        playingSong = model.getGlobalVariable(7,0)
+      else
+        playingSong = model.getGlobalVariable(7,0)
+      end
+      flushAudio()
+      model.setCustomFunction(62,{switch = sw1,func = 16,name = playlist[playingSong][2]})
+      model.setTimer(2,{value=0})
+    end
+  else
+    prevSongSwitchPressed = false
+  end	
 end
 
-thisPage.run=function(...)
-
-  if active then
-    page.run(...)
-    active= not (...==EVT_MENU_BREAK or ...==EVT_RIGHT_BREAK)
-  else  
-	-- INPUT HANDLING --
-	if (... == EVT_ROT_RIGHT or ... == EVT_MINUS_FIRST or ... == EVT_MINUS_RPT or ...== EVT_UP_BREAK) then
-		if playlistID == #playlists then
-			playlistID = 1
-		else
-			playlistID = playlistID + 1
-		end
-	elseif (... == EVT_ROT_LEFT or ... == EVT_PLUS_FIRST or ... == EVT_PLUS_RPT or ...== EVT_DOWN_BREAK) then
-		if playlistID == 1 then
-			playlistID = #playlists
-		else
-			playlistID = playlistID - 1
-		end
-	end	
-	local plf = playlists[playlistID]
-	if plf == nil then 
-		plf = playlists[1];
-	end
-	
-    loadScript(plf)() 
-
--- Select Screen
-	if LCD_W==212 then --9DX screen
-		lcd.clear()
-		lcd.drawText(LCD_W/3, 0, "SELECT PLAYLIST", BLINK,SMLSIZE) 
-		lcd.drawText(LCD_W/2-string.len(title)*4, 20, title, MIDSIZE)
-		lcd.drawText(LCD_W/3+17, 34,"Songs: " .. #playlist,SMLSIZE)
-		lcd.drawText(46, 57,"Scroll: [+/-] SELECT: [ENTER]",SMLSIZE)	
-	elseif shPresent== 0 then  --Xlite screen
-			lcd.clear()
-		lcd.drawText(LCD_W/6, 0, "SELECT PLAYLIST", BLINK,SMLSIZE) 
-		lcd.drawText(LCD_W/2-string.len(title)*4, 20, title, MIDSIZE)
-		lcd.drawText(LCD_W/3+7,32,"Songs: " .. #playlist,SMLSIZE)
-		lcd.drawText(0, 57,"Choose:[Joystick]/OK [ENTER]",SMLSIZE)
-	else --Q x7 Screen
-		lcd.clear()
-		lcd.drawText(LCD_W/6, 0, "SELECT PLAYLIST", BLINK,SMLSIZE) 
-		lcd.drawText(LCD_W/2-string.len(title)*4, 20, title, MIDSIZE)
-		lcd.drawText(LCD_W/3+7,32,"Songs: " .. #playlist,SMLSIZE)
-		lcd.drawText(0, 57,"Choose Scroll/Select:[ENTER]",SMLSIZE)	
-	end
-	clearTable(page)
-	if ... == EVT_ROT_BREAK or ... == EVT_ENTER_BREAK then
-		active = true
-		local f = io.open("itunes.conf", "w") 
-		io.write(f, playlistID)
-		io.close(f)
-		selectedPlaylistFile=playlists[playlistID]
-		model.setTimer(2,{value=0})
-	elseif ... == EVT_PAGE_BREAK then
-		active = true
-		end
-	
-	thisPage.init()
-	return not (...==EVT_MENU_BREAK or ...==EVT_RIGHT_BREAK)
-  end   
-end
-
-
-return thisPage
+return {run = run, background = background, init = init}
